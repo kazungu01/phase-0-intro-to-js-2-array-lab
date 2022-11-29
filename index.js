a4f1de0828484655058cd715357a487d367ec93b
hassan kazungu <hassankazungu01@gmail.com>
2022-11-29 14:58:40 +0300

    completed asignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(catname) {
+    cats.push(catname);
+}
+function destructivelyPrependCat(catname) {
+    cats.unshift(catname);
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+}
+function appendCat(catname){
+    const newcats = [...cats, catname];
+    return newcats;
+}
+function prependCat(catname){
+    const newcats = [catname, ...cats]
+    return newcats;
+}
+
+function removeLastCat(){
+    const newcats = cats.slice(0, -1);
+    return newcats;
+}
+function removeFirstCat(){
+    const newcats = cats.slice(1);
+    return newcats;
+}
